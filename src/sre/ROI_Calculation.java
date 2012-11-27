141fbc720623df2d61900c889085a45355aed95c
Ludo <ludovic.lavalette@gmail.com>
2012-11-27 01:26:17 -0500

    test

--- src/sre/ROI_Calculation.java
+++ src/sre/ROI_Calculation.java
@@ -1,13 +1,72 @@
 package sre;
 
-public class ROI_Calculation {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class ROI_Calculation {
+	
+	public static void parseFile(String nameFile, csv CSVFile)
+	{
+		try
+		{
+			CSVFile.file = new BufferedReader(new FileReader("csv/" + nameFile)); 
+
+			try 
+			{
+				String line;
+				String word_with_space;
+				String word_without_space;
+				while ((line = CSVFile.file.readLine()) != null) {
+					//System.out.println(line);
+					StringTokenizer stComma = new StringTokenizer(line, ",");
+					while (stComma.hasMoreTokens()) {
+						word_with_space = stComma.nextToken();
+					    //System.out.println(word_with_space);
+					    StringTokenizer stSpace = new StringTokenizer(word_with_space);
+					    word_without_space = stSpace.nextToken();
+					    //System.out.println(word_without_space);
+					    if(word_without_space.equals("Evaluation"))
+					    {
+					    	word_without_space = stSpace.nextToken();
+					    	//System.out.println(">>> " + word_without_space);
+					    	if (word_without_space.equals("Period:"))
+					    	{
+					    		word_without_space = stSpace.nextToken();
+					    		//System.out.println(word_without_space);
+					    		CSVFile.start = word_without_space;
+					    		System.out.println("start : " + CSVFile.start);
+					    		word_without_space = stSpace.nextToken();
+					    		word_without_space = stSpace.nextToken();
+					    		CSVFile.end = word_without_space;
+					    		System.out.println("end : " + CSVFile.end);
+					    	}
+					    }
+					}
+				}
+			}
+			finally 
+			{
+				CSVFile.file.close();
+			}
+		}
+		catch (IOException ioe)
+		{
+			System.out.println("Error --" + ioe.toString());
+		}
+		
+	}
+	
+	
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//String nameFile = args[1];
+		//System.out.println(nameFile);
+		csv CSVFile = new csv();
+		parseFile("test.csv", CSVFile);
 	}
 
 }
